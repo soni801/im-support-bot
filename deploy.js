112c70edded1e9c234d43189b1348e0a047d0d82
Soni <soni801_@outlook.com>
2021-10-04 20:50:51 +0200

    Get faq entires from upstream

--- deploy.js
+++ deploy.js
@@ -1,59 +1,56 @@
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const { clientId, token } = require('./config.json');
+const request = require("request");
 
-const choices = [
+const choices = [];
+request('https://help.yessness.com/assets/json/faq.json', (e, r, b) =>
+{
+    const faq = JSON.parse(b);
+    for (const [index, value] of faq.entries())
     {
-        "name": "Question 1",
-        "value": "some_value_1"
-    },
-    {
-        "name": "Question 2",
-        "value": "some_value_2"
-    },
-    {
-        "name": "Question 3",
-        "value": "some_value_3"
+        choices.push({"name":value.question,"value":index.toString()});
     }
-];
+});
 
-const commands = [
-    {
-        "name": "faq",
-        "description": "Interact with the FAQ",
-        "options": [
-            {
-                "type": 1,
-                "name": "search",
-                "description": "Search for a question in the FAQ",
-                "options": [
-                    {
-                        "type": 3,
-                        "name": "query",
-                        "description": "What to search for",
-                        "required": true
-                    }
-                ]
-            },
-            {
-                "type": 1,
-                "name": "get",
-                "description": "Specify a question to get the answer to",
-                "options": [
-                    {
-                        "type": 3,
-                        "name": "choice",
-                        "description": "Which question to get the answer to",
-                        "required": true,
-                        "choices": choices
-                    }
-                ]
-            }
-        ]
-    }
-];
+setTimeout(() =>
+{
+    const commands = [
+        {
+            "name": "faq",
+            "description": "Interact with the FAQ",
+            "options": [
+                {
+                    "type": 1,
+                    "name": "search",
+                    "description": "Search for a question in the FAQ",
+                    "options": [
+                        {
+                            "type": 3,
+                            "name": "query",
+                            "description": "What to search for",
+                            "required": true
+                        }
+                    ]
+                },
+                {
+                    "type": 1,
+                    "name": "get",
+                    "description": "Specify a question to get the answer to",
+                    "options": [
+                        {
+                            "type": 3,
+                            "name": "choice",
+                            "description": "Which question to get the answer to",
+                            "required": true,
+                            "choices": choices
+                        }
+                    ]
+                }
+            ]
+        }
+    ];
 
-const rest = new REST({ version: '9' }).setToken(token);
-
-rest.put(Routes.applicationGuildCommands(clientId, "755788845085229076"), { body: commands })
-    .then(() => console.log('Successfully deployed commands.'));
+    const rest = new REST({ version: '9' }).setToken(token);
+    rest.put(Routes.applicationGuildCommands(clientId, "755788845085229076"), { body: commands }).then(() => console.log('Successfully deployed commands.'));
+}, 100);
