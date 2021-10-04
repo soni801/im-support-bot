7f579641262a38cb4611d0d61d0393c6dc221cee
Soni <soni801_@outlook.com>
2021-10-04 21:11:48 +0200

    Respond properly to faq command

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const { token } = require("./config.json");
 const request = require('request');
 
 let faq;
-request('https://help.yessness.com/assets/json/faq.json', (e, r, b) => faq = b);
+request('https://help.yessness.com/assets/json/faq.json', (e, r, b) => faq = JSON.parse(b));
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
@@ -20,7 +20,47 @@ client.on("interactionCreate", interaction =>
         switch (interaction.commandName)
         {
             case "faq":
-                interaction.reply(faq.substr(0, 2000));
+                switch (interaction.options.getSubcommand())
+                {
+                    case "search":
+                        interaction.reply({
+                            embeds: [
+                                {
+                                    color: 0x57fAf8,
+                                    author: {
+                                        name: "FAQ Search",
+                                        icon_url: "https://cdn.discordapp.com/avatars/893457764871966729/0624aae7b4adf4cb6cb0cc2aeac23c48.webp"
+                                    },
+                                    fields: [
+                                        {
+                                            name: "Unavailable",
+                                            value: "This feature is not yet supported in TempleBot. We suggest using `/faq get` for the time being."
+                                        }
+                                    ]
+                                }
+                            ]
+                        });
+                        break;
+                    case "get":
+                        interaction.reply({
+                            embeds: [
+                                {
+                                    color: 0x57fAf8,
+                                    author: {
+                                        name: "FAQ Answer",
+                                        icon_url: "https://cdn.discordapp.com/avatars/893457764871966729/0624aae7b4adf4cb6cb0cc2aeac23c48.webp"
+                                    },
+                                    fields: [
+                                        {
+                                            name: faq[interaction.options.getString("choice")].question,
+                                            value: faq[interaction.options.getString("choice")].answer
+                                        }
+                                    ]
+                                }
+                            ]
+                        });
+                        break;
+                }
                 break;
         }
     }
