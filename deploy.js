82d9a4f14fa0521024368bcd9cab00b282f36f5d
Soni <soni801_@outlook.com>
2021-11-01 19:11:57 +0100

    Add ticket commands to deploy script

--- deploy.js
+++ deploy.js
@@ -17,33 +17,138 @@ setTimeout(() =>
 {
     const commands = [
         {
-            "name": "faq",
-            "description": "Interact with the FAQ",
-            "options": [
+            name: "faq",
+            description: "Interact with the FAQ",
+            options: [
                 {
-                    "type": 1,
-                    "name": "search",
-                    "description": "Search for a question in the FAQ",
-                    "options": [
+                    type: 1,
+                    name: "search",
+                    description: "Search for a question in the FAQ",
+                    options: [
                         {
-                            "type": 3,
-                            "name": "query",
-                            "description": "What to search for",
-                            "required": true
+                            type: 3,
+                            name: "query",
+                            description: "What to search for",
+                            required: true
                         }
                     ]
                 },
                 {
-                    "type": 1,
-                    "name": "get",
-                    "description": "Specify a question to get the answer to",
-                    "options": [
+                    type: 1,
+                    name: "get",
+                    description: "Specify a question to get the answer to",
+                    options: [
                         {
-                            "type": 3,
-                            "name": "choice",
-                            "description": "Which question to get the answer to",
-                            "required": true,
-                            "choices": choices
+                            type: 3,
+                            name: "choice",
+                            description: "Which question to get the answer to",
+                            required: true,
+                            choices: choices
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            name: "ticket",
+            description: "Create or modify a support ticket",
+            options: [
+                {
+                    type: 1,
+                    name: "create",
+                    description: "Create a support ticket",
+                    options: [
+                        {
+                            type: 3,
+                            name: "subject",
+                            description: "The subject of the ticket",
+                            required: true
+                        },
+                        {
+                            type: 3,
+                            name: "category",
+                            description: "The category of the ticket",
+                            required: true,
+                            choices: [
+                                {
+                                    name: "HTML",
+                                    value: "html"
+                                },
+                                {
+                                    name: "Styling (CSS)",
+                                    value: "css"
+                                },
+                                {
+                                    name: "JavaScript",
+                                    value: "js"
+                                },
+                                {
+                                    name: "Design",
+                                    value: "design"
+                                },
+                                {
+                                    name: "Other/unspecified",
+                                    value: "other"
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    type: 2,
+                    name: "modify",
+                    description: "Modify a support ticket",
+                    options: [
+                        {
+                            type: 1,
+                            name: "subject",
+                            description: "Modify the subject of a support ticket",
+                            options: [
+                                {
+                                    type: 4,
+                                    name: "id",
+                                    description: "The ID of the ticket to modify",
+                                    required: true
+                                },
+                                {
+                                    type: 3,
+                                    name: "subject",
+                                    description: "The new subject of the ticket",
+                                    required: true
+                                }
+                            ]
+                        },
+                        {
+                            type: 1,
+                            name: "category",
+                            description: "Modify the category of a support ticket",
+                            options: [
+                                {
+                                    type: 4,
+                                    name: "id",
+                                    description: "The ID of the ticket to modify",
+                                    required: true
+                                },
+                                {
+                                    type: 3,
+                                    name: "category",
+                                    description: "The new category of the ticket",
+                                    required: true
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    type: 1,
+                    name: "delete",
+                    description: "Delete a support ticket",
+                    options: [
+                        {
+                            type: 4,
+                            name: "id",
+                            description: "The ID of the ticket to remove",
+                            required: true
                         }
                     ]
                 }
