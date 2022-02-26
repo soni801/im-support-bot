3f8afacaa9b1e9c14e8f82040436c83e3282bb78
S1LV3R <s1lv3r@corax.team>
2022-02-27 00:14:40 +0100

    add better logging for ready event

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,7 +1,18 @@
 import { event } from '../types/event';
 
 const ready: event<'ready'> = async (client) => {
+  client.user?.setActivity({
+    type: 'WATCHING',
+    name: 'yall struggle to code',
+    url: 'https://haha.yessness.com',
+  });
+
   client.logger.warn(`Logged in as ${client.user?.tag}!`);
+  console.log('====================');
+  console.log(`Guilds: ${client.guilds.cache.size}`);
+  console.log(`Users (cached): ${client.users.cache.size}`);
+  console.log(`Channels: ${client.channels.cache.size}`);
+  console.log('====================');
 };
 
 export default ready;
